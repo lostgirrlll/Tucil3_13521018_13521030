3f70bef3ecbde482999034b10f694772c318df1f
jaulthfh <13521030@std.stei.itb.ac.id>
2023-04-09 18:53:30 +0700

    initial visualization

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 from inputFile import *
+from visualisasi import *
 
 node, matrix = inputValid()
 
 printList(node)
 printMatrix(matrix)
+createGraph(node, matrix)
+showGraph(createGraph(node, matrix))
---
+++ src/visualisasi.py
@@ -0,0 +1,23 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+from inputFile import *
+
+# membuat graph dari file inputFile.py
+def createGraph(node, matrix):
+    graph = nx.Graph()
+    for i in range(len(node)):
+        graph.add_node(node[i])
+    for i in range(len(node)):
+        for j in range(len(node)):
+            if matrix[i][j] != 0:
+                graph.add_edge(node[i], node[j], weight=matrix[i][j])
+    return graph
+
+# menampilkan graph
+def showGraph(graph):
+    position = nx.spring_layout(graph)
+    nx.draw(graph, position, with_labels=True, node_color='green', node_size=300, edge_color='black', width=2, font_size=8)
+    labels = nx.get_edge_attributes(graph, 'weight')
+    nx.draw_networkx_edge_labels(graph, position, edge_labels=labels, font_size=8, label_pos=0.5, rotate=False, font_color='red', font_weight='bold')
+    plt.show()
