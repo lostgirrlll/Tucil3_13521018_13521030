04f5a6c10101ac581afde853bac7b0d4ee8c713a
jaulthfh <13521030@std.stei.itb.ac.id>
2023-04-12 02:05:59 +0700

    modif showGraph

--- src/visualisasi.py
+++ src/visualisasi.py
@@ -15,13 +15,36 @@ def createGraph(node, matrix):
     return graph
 
 # menampilkan graph
-def showGraph(graph, arr):
-    listpost = []
+def showGraph(graph, arr, finalGraph, string, path, cost):
     position = nx.spring_layout(graph)
     position = arr
-    listpost.append(position)
-    nx.draw(graph, position, with_labels=True, node_color='green', node_size=300, edge_color='black', width=2, font_size=8)
-    labels = nx.get_edge_attributes(graph, 'weight')
-    nx.draw_networkx_edge_labels(graph, position, edge_labels=labels, font_size=8, label_pos=0.5, rotate=False, font_color='red', font_weight='bold')
+    if (string == "initial"):
+        nx.draw(graph, position, with_labels=True, node_color='green', node_size=300, edge_color='grey', width=2, font_size=8)
+        labels = nx.get_edge_attributes(graph, 'weight')
+        nx.draw_networkx_edge_labels(graph, position, edge_labels=labels, font_size=8, label_pos=0.5, rotate=False, font_color='black', font_weight='bold')
+        fig = plt.gcf()
+        fig.suptitle('Initial Graph', fontsize=16, fontweight='bold')
+    elif (string == "ucs"):
+        # print(finalGraph.edges)
+        # print(finalGraph)
+        edge_colors = ['red' if edge in finalGraph.edges else 'grey' for edge in graph.edges()]
+        nx.draw(graph, position, with_labels=True, node_color='green', node_size=300, edge_color=edge_colors, width=2, font_size=8)
+        labels = nx.get_edge_attributes(graph, 'weight')
+        nx.draw_networkx_edge_labels(graph, position, edge_labels=labels, font_size=8, label_pos=0.5, rotate=False, font_color='black', font_weight='bold')
+        for i in range(len(finalGraph.nodes)):
+            nx.draw_networkx_nodes(finalGraph, position, nodelist=[list(finalGraph.nodes)[i]], node_color='blue', node_size=500)
+        fig = plt.gcf()
+        fig.suptitle('UCS Graph', fontsize=16, fontweight='bold')
+        fig.text(0.02, 0.05, 'Path: ' + str(path), fontsize=5, fontweight='bold', color='black', bbox=dict(facecolor='white', edgecolor='black', boxstyle='round,pad=0.5'))
+        fig.text(0.02, 0.02, 'Cost: ' + str(cost), fontsize=5, fontweight='bold', color='black', bbox=dict(facecolor='white', edgecolor='black', boxstyle='round,pad=0.5'))
+    elif (string == "a*"):
+        edge_colors = ['red' if edge in finalGraph.edges else 'grey' for edge in graph.edges()]
+        nx.draw(graph, position, with_labels=True, node_color='green', node_size=300, edge_color=edge_colors, width=2, font_size=8)
+        labels = nx.get_edge_attributes(graph, 'weight')
+        nx.draw_networkx_edge_labels(graph, position, edge_labels=labels, font_size=8, label_pos=0.5, rotate=False, font_color='black', font_weight='bold')
+        for i in range(len(finalGraph.nodes)):
+            nx.draw_networkx_nodes(finalGraph, position, nodelist=[list(finalGraph.nodes)[i]], node_color='blue', node_size=500)
+        fig = plt.gcf()
+        fig.suptitle('A* Graph', fontsize=16, fontweight='bold')
+        fig.text(0.01, 0.01, 'Path: ' + str(path) + ' Cost: ' + str(cost), fontsize=8)
     plt.show()
-    return listpost
