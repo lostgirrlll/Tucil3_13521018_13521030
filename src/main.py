b9aa14d73c24b1c255c2c8c7a3a6942c3d3473ed
jaulthfh <13521030@std.stei.itb.ac.id>
2023-04-12 11:50:09 +0700

    update main

--- src/main.py
+++ src/main.py
@@ -1,16 +1,53 @@
 from inputFile import *
 from visualisasi import *
 from aStar import *
+from ucs import *
 
-node, point, matrix, matrixDist = inputValid()
-# printList(node)
-# printList(point)
-# printMatrix(matrix)
-# printMatrix(matrixDist)
-createGraph(node, matrix)
+if __name__ == "__main__":
+    art("SHORT PATH")
+    node, point, matrix, matrixDist = inputValid()
+    print("\t Menampilkan Graph...")
+    showGraph(createGraph(node, 0, matrix, "initial"), arrKoordinat(node, point), 0, "initial", 0, 0)
+    art("line")
+    print("\t Area :")
+    for i in range(len(node)):
+        print("\t -", node[i])
+    start = str(input("\t Masukkan titik awal : "))
+    goal = str(input("\t Masukkan titik tujuan : "))
+    while (start not in node or goal not in node or start == goal):
+        if (start == goal and start in node and goal in node):
+            print("\t Titik tidak valid. Titik awal tidak boleh sama dengan titik tujuan.")
+        else:
+            print("\t Titik tidak valid. Masukkan titik yang ada di area.")
+        start = str(input("\t Masukkan titik awal : "))
+        goal = str(input("\t Masukkan titik tujuan : "))
 
-showGraph(createGraph(node, matrix), arrKoordinat(node, point))
+    art("UCS")
+    path, cost = ucs(matrix, start, goal, node)
+    if (start not in path or goal not in path):
+        print("\t Tidak ada jalur yang dapat ditempuh.")
+    else:
+        print("\t Path : ", end="")
+        strpath = ""
+        for i in range(len(path) - 1):
+            strpath = strpath + path[i] + " -> "
+            print(path[i], end=" -> ")
+        print(goal)
+        print("\t Jarak : ", sum(cost))
+        print("\t Menampilkan Graph...")
+        mat = matrixPath(path, node, cost)
+        # print(matrixPath(path, node, cost))
+        showGraph(createGraph(node, 0, matrix, "initial"), arrKoordinat(node, point), createGraph(node, path, mat, "ucs_aStar"), "ucs", strpath + goal, sum(cost))
 
-path, dist = aStar(node, matrix, matrixDist)
-printList(path)
-print(dist)
+    art("A*")
+    pathA, costA, goalA = aStar(node, matrix, matrixDist)
+    print("\t Path : ", end="")
+    strpathA = ""
+    for i in range(len(pathA) - 1):
+        strpathA = strpathA + pathA[i] + " -> "
+        print(pathA[i], end=" -> ")
+    print(goalA)
+    print("\t Jarak : ", costA)
+    print("\t Menampilkan Graph...")
+    mat = matrixPath(pathA, node, cost)
+    showGraph(createGraph(node, 0, matrix, "initial"), arrKoordinat(node, point), createGraph(node, pathA, mat, "ucs_aStar"), "aStar", strpathA + goal, costA)
